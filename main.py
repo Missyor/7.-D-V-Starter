bfaf7da3ad582724e9cc8b45ad86cccfbdf0f67d
Aoife O'Rourke <aoife.orourke@hotmail.com>
2020-11-15 15:21:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,19 @@
+# Task 1 
+# Create code to print your own name 
+print("Aoife")
+
+ 
+
+# Task 2 
+# Create a variable called personAge that asks, and allows, the user to input in their age. Make sure the age is stored as a number 
+personAge = int(input("What age are you? "))
+ 
+
+# Task 3 
+# Create a variable called personAge that asks, and allows, the user to input in their age. Make sure the age is stored as a number. Create a new variable called dogYears. When dogYears is printed, we want it to calculate a person's age in dog years. This should be the person's age multiplied by 7. 
+personAge = int(input("What age are you? "))
+dogYears = personAge * 7
+print(dogYears)
+
+# Task 4 
+# Repeat Task 3 but, this time, write code so that if you are given a dog's age you can find their human age
